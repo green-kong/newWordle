d0e450cc3de4742ba74602dd881d54ef5319cddd
dev_kong <ahdjd5@gmail.com>
2022-05-12 19:14:35 +0900

    deploy

--- prac/api/user/login.js
+++ prac/api/user/login.js
@@ -2,17 +2,17 @@ import axios from 'axios';
 
 axios.defaults.baseURL =
   process.env.NODE_ENV === 'production'
-    ? '43.200.18.7'
+    ? 'http://43.200.15.189'
     : 'http://localhost:4000';
 
 export const loginAPI = async (payload) => {
-  const url = '/api/user/login';
+  const url = 'api/user/login';
   const response = await axios.post(url, payload);
   return response.data;
 };
 
 export const verifyAPI = async (payload) => {
-  const url = '/api/user/me';
+  const url = 'api/user/me';
   const option = {
     headers: {
       Authorization: `Bearer ${payload.token}`,
@@ -23,7 +23,7 @@ export const verifyAPI = async (payload) => {
 };
 
 export const joinAPI = async (payload) => {
-  const url = '/api/user/join';
+  const url = 'api/user/join';
   const response = await axios.post(url, payload);
   return response.data;
 };
